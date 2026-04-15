73444963f6a36b03a29c6cc654243cce54b51afb
André Furchner <storkner@gmail.com>
2026-04-15 22:10:52 +0200

    compile localy

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use getrandom::getrandom;
+use rand::Rng;
+use std::mem;
 use wasm_bindgen::prelude::*;
 
+#[repr(C)] // Ensure a predictable C-like layout in memory (x followed by y)
 #[derive(Debug, Clone)]
-struct Vec2 {
+pub struct Vec2 {
     x: f64,
     y: f64,
 }
@@ -13,48 +15,64 @@ impl Vec2 {
     }
 }
 
-fn get_random_f64_raw() -> f64 {
-    let mut buf = [0u8; 8];
-    getrandom(&mut buf).unwrap();
-    f64::from_ne_bytes(buf)
+#[wasm_bindgen]
+pub struct Simulation {
+    buffer_a: Vec<Vec2>,
+    buffer_b: Vec<Vec2>,
 }
 
 #[wasm_bindgen]
-pub fn work(array_size: usize) {
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new(size: usize) -> Self {
+        let mut rng = rand::thread_rng();
+        let buffer_a: Vec<Vec2> = (0..size)
+            .map(|_| Vec2 {
+                x: rng.r#gen::<f64>() - 0.5,
+                y: rng.r#gen::<f64>() - 0.5,
+            })
+            .collect();
 
+        let buffer_b: Vec<Vec2> = vec![Vec2 { x: 0.0, y: 0.0 }; size];
 
-    let mut buffer_a: Vec<Vec2> = (0..array_size)
-        .map(|_| Vec2 {
-            x: get_random_f64_raw() - 0.5,
-            y: get_random_f64_raw() - 0.5,
-        })
-        .collect();
-
-    let mut buffer_b: Vec<Vec2> = vec![Vec2 { x: 0.0, y: 0.0 }; array_size];
-
-    for _ in 0..128 {
-        for x in 0..1024 {
-            for y in 0..1024 {
-                buffer_b[x].x = 0.0;
-                buffer_b[x].y = 0.0;
-
-                if x == y {
-                    continue;
-                }
-                // distance
-                let f = 1.0 / buffer_a[x].distance_to(&buffer_a[y]).powi(2);
-                buffer_b[x].x += (buffer_a[x].x - buffer_a[y].x) * f;
-                buffer_b[x].y += (buffer_a[x].y - buffer_a[y].y) * f;
-            }
-            buffer_b[x].x = buffer_a[x].x + buffer_b[x].x;
-            buffer_b[x].y = buffer_a[x].y + buffer_b[x].y;
-        }
-
-        // swap
-        let tmp = buffer_a;
-        buffer_a = buffer_b;
-        buffer_b = tmp;
+        Simulation { buffer_a, buffer_b }
     }
 
-    println!("{:?}", buffer_a[0]);
+    pub fn len(&self) -> usize {
+        self.buffer_a.len()
+    }
+
+    pub fn data_ptr(&self) -> *const Vec2 {
+        self.buffer_a.as_ptr()
+    }
+
+    pub fn size(&self) -> usize {
+        size_of::<Vec2>()
+    }
+
+    pub fn work(&mut self) {
+        let size = self.buffer_a.len();
+
+        for _ in 0..128 {
+            for x in 0..size {
+                for y in 0..size {
+                    self.buffer_b[x].x = 0.0;
+                    self.buffer_b[x].y = 0.0;
+
+                    if x == y {
+                        continue;
+                    }
+                    // distance
+                    let f = 1.0 / self.buffer_a[x].distance_to(&self.buffer_a[y]).powi(2);
+                    self.buffer_b[x].x += (self.buffer_a[x].x - self.buffer_a[y].x) * f;
+                    self.buffer_b[x].y += (self.buffer_a[x].y - self.buffer_a[y].y) * f;
+                }
+                self.buffer_b[x].x = self.buffer_a[x].x + self.buffer_b[x].x;
+                self.buffer_b[x].y = self.buffer_a[x].y + self.buffer_b[x].y;
+            }
+
+            // swap
+            mem::swap(&mut self.buffer_a, &mut self.buffer_b);
+        }
+    }
 }
