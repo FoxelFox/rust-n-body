1f57cb7d0b6d725aa6468605e1728790b17b1ff2
André Furchner <storkner@gmail.com>
2026-04-26 12:48:02 +0200

    multitreading

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
 use rand::Rng;
+use rayon::prelude::*;
 use std::mem;
 
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-#[cfg(target_arch = "wasm32")]
-use web_sys::window;
-
 #[repr(C)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Vec2 {
     pub x: f64,
     pub y: f64,
@@ -19,15 +15,12 @@ impl Vec2 {
     }
 }
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 pub struct Simulation {
     buffer_a: Vec<Vec2>,
     buffer_b: Vec<Vec2>,
 }
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 impl Simulation {
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(constructor))]
     pub fn new(size: usize) -> Self {
         let mut rng = rand::thread_rng();
         let buffer_a: Vec<Vec2> = (0..size)
@@ -51,48 +44,30 @@ impl Simulation {
     }
 
     pub fn size(&self) -> usize {
-        size_of::<Vec2>()
+        std::mem::size_of::<Vec2>()
     }
 
     pub fn work(&mut self) {
-        let size = self.buffer_a.len();
-
         for _ in 0..128 {
-            for x in 0..size {
-                for y in 0..size {
-                    self.buffer_b[x].x = 0.0;
-                    self.buffer_b[x].y = 0.0;
-
-                    if x == y {
-                        continue;
+            let a = &self.buffer_a;
+            self.buffer_b
+                .par_iter_mut()
+                .enumerate()
+                .for_each(|(x, out)| {
+                    let mut acc = Vec2 { x: 0.0, y: 0.0 };
+                    for y in 0..a.len() {
+                        if x == y {
+                            continue;
+                        }
+                        let f = 1.0 / a[x].distance_to(&a[y]).powi(2);
+                        acc.x += (a[x].x - a[y].x) * f;
+                        acc.y += (a[x].y - a[y].y) * f;
                     }
-                    let f = 1.0 / self.buffer_a[x].distance_to(&self.buffer_a[y]).powi(2);
-                    self.buffer_b[x].x += (self.buffer_a[x].x - self.buffer_a[y].x) * f;
-                    self.buffer_b[x].y += (self.buffer_a[x].y - self.buffer_a[y].y) * f;
-                }
-                self.buffer_b[x].x = self.buffer_a[x].x + self.buffer_b[x].x;
-                self.buffer_b[x].y = self.buffer_a[x].y + self.buffer_b[x].y;
-            }
+                    out.x = a[x].x + acc.x;
+                    out.y = a[x].y + acc.y;
+                });
 
             mem::swap(&mut self.buffer_a, &mut self.buffer_b);
         }
     }
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen(start)]
-pub fn main() {
-    let win = window().unwrap();
-    let perf = win.performance().unwrap();
-    let doc = win.document().unwrap();
-    let body = doc.body().unwrap();
-
-    let n = 2048;
-    let mut sim = Simulation::new(n);
-
-    let t0 = perf.now();
-    sim.work();
-    let elapsed = perf.now() - t0;
-
-    body.set_inner_text(&format!("{:.0} ms", elapsed));
 }
